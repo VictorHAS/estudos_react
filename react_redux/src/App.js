80a8c5ea1fa298298fcb99a9e92849c8030ccda2
VictorHAS <victorhermesalves@gmail.com>
2019-07-29 23:30:56 -0300

    React_Redux: primeiro contato

---
+++ react_redux/src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+import { Provider } from "react-redux";
+
+import Video from "./components/Video";
+import Sidebar from "./components/Sidebar";
+
+import store from "./store";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Provider store={store}>
+          <Video />
+          <Sidebar />
+        </Provider>
+      </div>
+    );
+  }
+}
+
+export default App;
